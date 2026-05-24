e0a1147dfd9863db3820a629dec71950d51e8cb4
Talha Zaheer <p230060@pwr.nu.edu.pk>
2026-05-24 09:36:01 +0500

    Ho gaya

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from ml_integration import get_recommendations, get_recommendation_strategies, g
 import mysql.connector
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key_here'
+app.secret_key = ''
 
 
 # ==================== HELPER FUNCTIONS ====================
